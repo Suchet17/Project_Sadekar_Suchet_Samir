58049861827c09c052ea8d28518ebe784a2bb93f
Kanil <Sharva.ha@proton.me>
2025-04-27 00:11:23 +0530

    Correct predict.py return

--- predict.py
+++ predict.py
@@ -5,25 +5,24 @@ from dataset import PokemonDataset
 from model import PokemonCNN
 from config import image_dir
 
-def predictor(file_paths):
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+def predictor(file_names):
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
     model = PokemonCNN().to(device)
     model.load_state_dict(torch.load("checkpoints/final_weights.pth", weights_only=True))
+
     model.eval()
 
-    test_dataset = PokemonDataset(image_dir, file_paths)
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-    log = open("temp.txt", 'w')
-    acc = 0
+    test_dataset = PokemonDataset(image_dir, file_names)
+    test_loader = DataLoader(test_dataset, batch_size = 1, shuffle=True)
+    labs = []
     for i, (images, labels) in enumerate(test_loader):
         images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
-        outputs = torch.exp(outputs)/torch.sum(torch.exp(outputs), dim=1, keepdim=True)
-        # outputs = torch.softmax(outputs, dim=1)
-        if torch.argmax(outputs).item() == labels.item():
-            acc += 1
-    acc /= len(test_loader)
-    log.close()
-
-predictor()
+        outputs = torch.exp(outputs)/torch.sum(torch.exp(outputs), dim=1, keepdim=True) # softmax
+        labs.append(outputs.argmax(dim=1).cpu().numpy())
+    return labs
